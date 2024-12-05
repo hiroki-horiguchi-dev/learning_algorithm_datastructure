acbc388bf4211a201a96fd120cfc17d42cd57d98
busker <165013324+hiroki-horiguchi-dev@users.noreply.github.com>
2024-12-05 13:03:39 +0900

    2回目
    shiftJis にしないと読めなかったけど push するときは utf-8 で保存しないと化けるの勘弁してえ

--- intermediate/data_structure_algorithm_edition_two/package/exercise/heap/Heap.java
+++ intermediate/data_structure_algorithm_edition_two/package/exercise/heap/Heap.java
@@ -4,72 +4,76 @@ public class Heap {
 	int[] data = new int[1000];
 	int last = -1;
 
-	// �q�[�v�ւ̗v�f�̒ǉ�
+	// ヒープへの要素の追加
 	void insert(int object){
 		
-		// �Ō���ɗv�f��ǉ� (�������R�[�h)
+		// 最後尾に要素を追加 (正しいコード)
 		last = last + 1;
 		data[last] = object;		
 
 		
-		// ��ւƒH��Ȃ���t�]���������Ă���
+		// 上へと辿りながら逆転を解消していく
 		//
-		//	�����ɃR�[�h������
-		//
+		//	ここにコードを書く
+		// 1回目?
+		// 2回目?
 		int i = last;
 		while (i > 0) {
-			if (data[(i-1)/2] < data[i]) {
+			if (data[i] < data[i-1/2]) {
 				swap(data, (i-1)/2, i);
 				i = (i-1)/2;
+			} else {
+				return;
 			}
 		}
 
 		
-		// �ȉ��A�_�~�[�̃R�[�h�@�e�ƂЂƂ�������ւ���
+		// 以下、ダミーのコード　親とひとつだけ入れ替える
 		if (last >= 1){
-			Main.print(this, (last-1)/2, last);	//�@�\��
-			swap(data, (last-1)/2, last);	//����ւ�
+			Main.print(this, (last-1)/2, last);	//　表示
+			swap(data, (last-1)/2, last);	//入れ替え
 		}
 		
 	}
 	
 	
-	// �ŏ��l�̎��o��
+	// 最小値の取り出し
 	int deletemin(){
 		
-		//���̗v�f�����o�� (�������R�[�h)
+		//根の要素を取り出す (正しいコード)
 		int object = data[0];
 		
-		// �Ō�������Ɏ����Ă���  (�������R�[�h)
+		// 最後尾を根に持ってくる  (正しいコード)
 		data[0] = data[last];
 		last = last - 1;	
 
 		
-		//���ւƒH��Ȃ���t�]���������Ă��B
+		//下へと辿りながら逆転を解消してく。
 		//
-		//	�����ɃR�[�h������
-		//
+		//	ここにコードを書く
+		// 1回目❌
+		// 2回目✅
+		// テストケースとして考えられるのは以下の配列(元々の宣言から考えなくて良いケースもあるが、コードテスト上であれば必須なので書いた)
+		// []: 空配列
+		// [1]: 根のみの配列
+		// [1,2]: 根と1つの葉を持つ配列
+		// [1,2,3]: 根と2つの葉を持つ配列
+		// [1,2,3,4,5,6,7,8,9]: 1つの根と2つの葉を持つ構造が複数個ある.全体の要素数が奇数個
+		// [1,2,3,4,5,6,7,8,9,10]: 1つの根と2つの葉を持つ構造が複数個ある.全体の要素数が偶数個
+		// [1,1,1,1,1]: 要素が全て同じ数の配列
 		int i = 0;
-		// last/2 �Ń��[�v���q�����m�[�h�Ɍ��肷��
 		while (i < last/2) {
-			// �����ł܂Ƃ߂Ďg���ϐ����`���Ă��܂��ق����ǂ݂₷���Ǝv��
-			// �q�[�v���񕪖؂������Ă���揈����.�ƌ����Ӑ}���`��邩��
-			int parent = data[i];
-			// ���E�O�A�N�Z�X�� ArrayIndexOutOfBoundsException ����������\��������
-			// ���A�𓚃R�[�h�ɂ��̎|�̋L�ڂ��Ȃ����߈�U���̂܂܂Ƃ��� 
-			int left = data[(i*2) + 1];
-			int right = data[(i*2) + 2];
-			if (parent > left) {
-				if (left > right) {
-					swap(data, i, i*2+2);
-					i = (2*i) + 2;
-				} else {
-					swap(data, i, i*2+1);
-					i = (2*i) + 1;
+			if (data[i] > data[i*2 + 1]) {
+				if (data[i*2 + 1] > data[i*2 + 2]) {
+					swap(data, i, i*2 + 2);
+					i = i*2 + 2;
+				} 
+				else {
+					swap(data, i, i*2 + 1);
+					i = i*2 + 1;
 				}
-			} else if(parent > right) {
-				swap(data, i, i*2+2);
-				i = (2*i) + 2;
+			} else if (data[i] > data[i*2 + 2]) {
+				swap(data, i, i*2 + 2);	
 			} else {
 				return object;
 			}
@@ -77,7 +81,7 @@ public class Heap {
 		return object;
 	} 
 	
-	// A��i�Ԗڂ�j�Ԗڂ̗v�f�����ւ���(�������R�[�h)
+	// Aのi番目とj番目の要素を入れ替える(正しいコード)
 	static void swap(int[] A, int i, int j){
 		int tmp = A[i];
 		A[i] = A[j];
@@ -86,61 +90,61 @@ public class Heap {
 }
 /*
 
-�����
+正解例
 
 public class Heap {
 	int[] data = new int[1000];
 	int last = -1;
 
-	// �q�[�v�ւ̗v�f�̒ǉ�
+	// ヒープへの要素の追加
 	void insert(int object){
 		
-		// �Ō���ɗv�f��ǉ� (�������R�[�h)
+		// 最後尾に要素を追加 (正しいコード)
 		last = last + 1;
 		data[last] = object;		
 
 		
-		// ��ւƒH��Ȃ���t�]���������Ă���
+		// 上へと辿りながら逆転を解消していく
 		//
-		//	�����ɃR�[�h������
+		//	ここにコードを書く
 		//
 
 		
-		// �ȉ��A�_�~�[�̃R�[�h�@�e�ƂЂƂ�������ւ���
+		// 以下、ダミーのコード　親とひとつだけ入れ替える
 		if (last >= 1){
-			Main.print(this, (last-1)/2, last);	//�@�\��
-			swap(data, (last-1)/2, last);	//����ւ�
+			Main.print(this, (last-1)/2, last);	//　表示
+			swap(data, (last-1)/2, last);	//入れ替え
 		}
 		
 	}
 	
 	
-	// �ŏ��l�̎��o��
+	// 最小値の取り出し
 	int deletemin(){
 		
-		//���̗v�f�����o�� (�������R�[�h)
+		//根の要素を取り出す (正しいコード)
 		int object = data[0];
 		
-		// �Ō�������Ɏ����Ă���  (�������R�[�h)
+		// 最後尾を根に持ってくる  (正しいコード)
 		data[0] = data[last];
 		last = last - 1;	
 
 		
-		//���ւƒH��Ȃ���t�]���������Ă��B
+		//下へと辿りながら逆転を解消してく。
 		//
-		//	�����ɃR�[�h������
+		//	ここにコードを書く
 		//
 
 		
-		// �ȉ��A�_�~�[�̃R�[�h�@�q�ƂЂƂ�������ւ���
+		// 以下、ダミーのコード　子とひとつだけ入れ替える
 		int i = 0;
-		Main.print(this, i, i*2+2);	//�@�\��
-		swap(data, i, i*2+2);	//�E�̎q�ƌ���
+		Main.print(this, i, i*2+2);	//　表示
+		swap(data, i, i*2+2);	//右の子と交換
 
 		return object;
 	} 
 	
-	// A��i�Ԗڂ�j�Ԗڂ̗v�f�����ւ���(�������R�[�h)
+	// Aのi番目とj番目の要素を入れ替える(正しいコード)
 	static void swap(int[] A, int i, int j){
 		int tmp = A[i];
 		A[i] = A[j];
