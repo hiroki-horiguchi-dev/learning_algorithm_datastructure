0fd636783c47149dc3ea12957a0844dbfdc02a8d
busker <165013324+hiroki-horiguchi-dev@users.noreply.github.com>
2024-12-19 14:03:10 +0900

    5回目
    完全一致のプログラムを書けたので完璧！明日もう一度復習しておkなら次へ進む

--- intermediate/data_structure_algorithm_edition_two/package/exercise/binarysearchtree/BinarySearchTree.java
+++ intermediate/data_structure_algorithm_edition_two/package/exercise/binarysearchtree/BinarySearchTree.java
@@ -32,11 +32,63 @@ public class BinarySearchTree {
 	}
 
 	Node delete(Node node, int object) {
-		// 5回目 
+		// 5回目 ✅
+		// 考えるケースで重要なのは、削除対象ノードが葉なのか、子を一つ持つ枝なのか、子を二つ持つ枝なのか、だけ
+		if (node == null) return null;
+
+		if (object < node.object) {
+			// 左辿る、削除なのでnodeの左を更新していく
+			node.left = delete(node.left, object);
+			return node;
+		} else if (object > node.object) {
+			// 右辿る、同様に node の右を更新
+			node.right = delete(node.right, object);
+			return node;
+		}
+		else {
+			// 削除対象ノード
+			// 上記で述べた場合わけをここで気にする必要がある
+			if (node.left == null && node.right == null) {
+				// 削除対象なし、easy
+				return null;
+			} else if (node.left == null && node.right != null) {
+				// 削除対象が右に子を一つ持つ
+				return node.right;
+			} else if (node.left != null && node.right == null) {
+				// 削除対象が左に子を一つ持つ
+				return node.left;
+			} else {
+				// いちばん厄介なパターン
+				// 両方に子がいるので最小値を取得して、その最小値の左右を削除対象ののノード左右の子とつげ変えて返却すれば良い
+				Node min = deletemin(node.right, node);
+				min.left = node.left;
+				min.right = node.right;
+				return min;
+			}
+		}
+		
 	}
 
 	Node deletemin(Node node, Node parent){	// 子孫のうちの最小値を返却する
-		// 5回目 
+		// 5回目 ✅
+		if (node.left == null) { // 自身が最小値パターン
+			if (node == parent.left) {
+				parent.left = node.right;
+			} else {
+				parent.right = node.right;
+			}
+			return node;
+		}
+
+		// 削除対象の右子ノードの左の子ノードが無くなるまで探索して返却
+		parent = node;
+		node = node.left;
+		while (node.left != null) {
+			parent = node;
+			node = node.left;
+		}
+		parent.left = node.right;
+		return node;
 	}	
 }
 
